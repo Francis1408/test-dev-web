76c3900b3d85e9be47a9281a48bed6bb3acb7c70
Francisco <franciscoabreu1408@gmail.com>
2021-05-02 16:50:59 -0300

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,27 @@
-function Home(){
-    return <div>Home</div>
+import { useState } from 'react';
 
+function Home(){
+    return(
+        <div>
+            <h4>Home 4</h4>
+            <Contador />
+            <div>Teste</div>
+        </div>
+    )
+}
+function Contador(){
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador(){
+        setContador(contador + 1)
+    }
+
+    return(
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adcionar</button>
+        </div>
+    )
 }
 
 export default Home
